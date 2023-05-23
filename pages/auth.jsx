b614ed0da4f25defca26673378955e8506d0b2a8
Sugam Pokhrel <psugam75@gmail.com>
2023-05-23 08:46:34 +0545

    added auth properly

--- pages/auth.jsx
+++ pages/auth.jsx
@@ -2,7 +2,8 @@ import React, { useEffect } from 'react'
 import { useSession, signIn, signOut } from "next-auth/react"
 import Login from '../compos/Login/Login'
 import { databases } from '../appwrite'
-function auth() {
+import { ID } from 'appwrite';
+ function auth() {
     const { data: session, status } = useSession()
     if (status === "loading") return <p>Loading...</p>
     if (status === "error") return <p>{error.message}</p>
@@ -13,28 +14,52 @@ function auth() {
     }
     else {
         console.log(session.user.email)
-        const promise = databases.listDocuments('6468f10e6e9b67980c51', '6468f11ef0b4d9ad1d9e');
+    const promise = databases.listDocuments('6468f10e6e9b67980c51', '646c2809265ac09c5196');
 
     promise.then(function (response) {
     let  docs=response.documents
-    docs.map(doc=>{
-        if(!session.user.email==doc.email){
-            const promise = databases.createDocument('6468f10e6e9b67980c51', '6468f11ef0b4d9ad1d9e',  ID.unique(), {name:session.user.name,email:session.user.email,img:session.user.image});
-            promise.then(function (response) {
-    console.log(response); // Success
+    console.log(docs)
+
+ const  emailToFind = session.user.email;
+
+const  emailExists = docs.some(obj => obj.email === emailToFind);
+
+if (  emailExists) {
+  console.log("Email exists in the array");
+} else  {
+  console.log("Email does not exist in the array");
+  const promise= databases.createDocument('6468f10e6e9b67980c51', '646c2809265ac09c5196',  ID.unique(), {name:session.user.name,email:session.user.email,image:session.user.image})
+
+  promise.then(function (response) {
+   
 }, function (error) {
-    console.log(error); // Failure
+ 
 });
+}
+    // let isUser=false;
+    // docs.map(doc=>{
+    //     if(session.user.email==doc.email){
+    //         isUser=true
+           
 
+    //     }
 
-        } else{
-            console.log("user already in db")
-        }
+    // console.log(isUser)    
+
+//         if(!isUser){
+//              const promise = databases.createDocument('6468f10e6e9b67980c51', '6468f11ef0b4d9ad1d9e',  ID.unique(), {name:session.user.name,email:session.user.email,image:session.user.image});
+//             promise.then(function (response) {
+//     console.log(response); // Success
+// }, function (error) {
+//     console.log(error); // Failure
+// });
+
+//         } 
     })
     
-}, function (error) {
-    console.log(error); // Failure
-});
+// }, function (error) {
+//     console.log(error); // Failure
+// });
 
         
 
