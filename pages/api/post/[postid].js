6548e4425e7f9a93928581cbc2b7c24ab77e39ba
Sugam Pokhrel <psugam75@gmail.com>
2023-05-26 14:49:03 +0545

    added auth to api for put and delete

--- pages/api/post/[postid].js
+++ pages/api/post/[postid].js
@@ -1,13 +1,87 @@
 import { databases } from '../../../appwrite'
+
+import { getSession } from "next-auth/react";
+
 export default async function handler(req, res) {
+
+    var session = await getSession({ req })
+
+
+
+    if (!session) {
+        return res.status(401).json({ error: "Not Authenticated" });
+    }
+
+
+
+
     const {postid}=req.query;
+
+    //this is the get
 
     const promise = databases.getDocument('646ed509771c8bf97447', '646ed512bc1b4def6d45', postid);
 
     promise.then(function (response) {
-    res.send(response); // Success
+
+        let userData=response.createdBy;
+
+         if (req.method === 'GET') {
+
+             res.send(response);
+
+         }
+    // Success
 }, function (error) {
     console.log(error); // Failure
 });
+
+    
+
+
+    if (req.method === 'PUT') {
+        if(session.user.email!=userData){
+            return res.status(401).json({ error: "Not Authenticated" });
+
+        }
+
+        const {formdata}=req.query; //data must be passed as form data
+
+
+    // Process a PUT request
+
+const promise = databases.updateDocument('646ed509771c8bf97447', '646ed512bc1b4def6d45', postid,{formdata});
+
+promise.then(function (response) {
+    res.send(response) // Success this is the updated document
+}, function (error) {
+    console.log(error); // Failure
+});
+
+  } 
+
+
+    if (req.method === 'DELETE') {
+
+                if(session.user.email!=userData){
+            return res.status(401).json({ error: "Not Authenticated" });
+
+        }
+     
+
+
+    // Process a Delete request
+
+    const promise = databases.deleteDocument('646ed509771c8bf97447', '646ed512bc1b4def6d45', postid);
+
+promise.then(function (response) {
+    res.send({msg:"deleted Successfully"}) // Success this is the updated document
+}, function (error) {
+    console.log(error); // Failure
+});
+
+  } 
+
+
+
 
 }
