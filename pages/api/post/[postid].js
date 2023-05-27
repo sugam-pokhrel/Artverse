39332b29904b89fb0e13cdc3148e7bf303cfb796
meprazhant <meprazhant@gmail.com>
2023-05-27 21:54:38 +0545

    improved

--- pages/api/post/[postid].js
+++ pages/api/post/[postid].js
@@ -1,4 +1,3 @@
-import { json } from 'express';
 import { databases } from '../../../appwrite'
 
 import { getSession } from "next-auth/react";
@@ -16,7 +15,7 @@ export default async function handler(req, res) {
 
 
 
-    const {postid}=req.query;
+    const { postid } = req.query;
 
     //this is the get
 
@@ -24,63 +23,63 @@ export default async function handler(req, res) {
 
     promise.then(function (response) {
 
-        let userData=response.createdBy;
-      
+        let userData = response.createdBy;
 
-         if (req.method === 'GET') {
+
+        if (req.method === 'GET') {
             res.send(response)
 
-         }
-    // Success
-}, function (error) {
-   res.send({msg:"no docs found"}); // Failure
-});
+        }
+        // Success
+    }, function (error) {
+        res.send({ msg: "no docs found" }); // Failure
+    });
 
-    
-//for update
+
+    //for update
 
     if (req.method === 'PUT') {
-        if(session.user.email!=userData){
+        if (session.user.email != userData) {
             return res.status(401).json({ error: "Not Authenticated" });
 
         }
 
-        const {formdata}=req.query; //data must be passed as form data
+        const { formdata } = req.query; //data must be passed as form data
 
 
-    // Process a PUT request
+        // Process a PUT request
 
-const promise = databases.updateDocument('646ed509771c8bf97447', '646ed512bc1b4def6d45', postid,formdata);
+        const promise = databases.updateDocument('646ed509771c8bf97447', '646ed512bc1b4def6d45', postid, formdata);
 
-promise.then(function (response) {
-    res.send(response) // Success this is the updated document
-}, function (error) {
-   res.status(400).json({msg:"document not found"}) // Failure
-});
+        promise.then(function (response) {
+            res.send(response) // Success this is the updated document
+        }, function (error) {
+            res.status(400).json({ msg: "document not found" }) // Failure
+        });
 
-  } 
+    }
 
-//for delete
+    //for delete
     if (req.method === 'DELETE') {
 
-                if(session.user.email!=userData){
+        if (session.user.email != userData) {
             return res.status(401).json({ error: "Not Authenticated" });
 
         }
-     
 
 
-    // Process a Delete request
 
-    const promise = databases.deleteDocument('646ed509771c8bf97447', '646ed512bc1b4def6d45', postid);
+        // Process a Delete request
 
-promise.then(function (response) {
-    res.send({msg:"deleted Successfully"}) // Success this is the updated document
-}, function (error) {
-   res.status(400).json({msg:"document not found"}) // Failure
-});
+        const promise = databases.deleteDocument('646ed509771c8bf97447', '646ed512bc1b4def6d45', postid);
 
-  } 
+        promise.then(function (response) {
+            res.send({ msg: "deleted Successfully" }) // Success this is the updated document
+        }, function (error) {
+            res.status(400).json({ msg: "document not found" }) // Failure
+        });
+
+    }
 
 
 
