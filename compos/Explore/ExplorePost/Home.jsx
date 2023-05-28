364f612f7e492a2b5cf7445028145968e3ca408f
Sugam Pokhrel <psugam75@gmail.com>
2023-05-28 14:51:48 +0545

    changed like for some days to prevent eror

--- compos/Explore/ExplorePost/Home.jsx
+++ compos/Explore/ExplorePost/Home.jsx
@@ -1,52 +1,79 @@
-import React from 'react'
-import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai'
-import { FaRegSave } from 'react-icons/fa'
-import Thoughts from './Thoughts'
-import AddThought from './AddThought'
-
+import React from 'react';
+import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
+import { FaRegSave } from 'react-icons/fa';
+import Thoughts from './Thoughts';
+import AddThought from './AddThought';
+import { useSession } from 'next-auth/react';
 
 function Home({ post, user }) {
+  const { data: session } = useSession();
 
-    function likePost() {
-        var url = "/api/likes/" + post.$id
-        console.log(url)
-        fetch(url, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-            },
+  function likePost() {
+    var url = "/api/likes/" + post.$id;
+
+    if (session) {
+      fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ likedby: session.user.email }), // Pass session email in the request body
+      })
+        .catch(error => {
+          console.error('An error occurred while liking the post:', error);
+        });
+    } else {
+      console.log(url);
+      fetch(url, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then(res => {
+          const contentType = res.headers.get('content-type');
+          if (contentType && contentType.includes('application/json')) {
+            return res.json();
+          } else {
+            return 0;
+          }
         })
-            .then(res => res.json())
-            .then(data => { console.log(data) })
+        .then(data => {
+          console.log(data.length);
+        })
+        .catch(error => {
+          console.error('An error occurred while liking the post:', error);
+        });
     }
+  }
 
-    return (
-        <div className='explorePost'>
-            <div className="ep-top">
-                <div className="ep-user flex ">
-                    <img src={user?.image} alt="" />
-                    <div className="ep-u-data">
-                        <p>{post.title}</p>
-                        <div>{user?.name} . <span>Follow</span></div>
-                    </div>
-                </div>
-                <div className="ep-u-actions flex gap-5">
-                    <div className="btn btn-primary" onClick={likePost}><AiOutlineHeart size={22} />Like</div>
-                    <div className="btn btn-warning"><FaRegSave size={22} />Save</div>
-                </div>
-            </div>
-            <div className="ep-img">
-                <img src={post.image} alt="" />
-            </div>
-            <div className="ep-desc">
-                <p>{post.desc}</p>
-            </div>
-            <div className="ep-thoughts">
-                <AddThought />
-                <Thoughts />
-            </div>
+  return (
+    <div className='explorePost'>
+      <div className="ep-top">
+        <div className="ep-user flex">
+          <img src={user?.image} alt="" />
+          <div className="ep-u-data">
+            <p>{post.title}</p>
+            <div>{user?.name} . <span>Follow</span></div>
+          </div>
         </div>
-    )
+        <div className="ep-u-actions flex gap-5">
+          <div className="btn btn-primary" onClick={likePost}><AiOutlineHeart size={22} />Like</div>
+          <div className="btn btn-warning"><FaRegSave size={22} />Save</div>
+        </div>
+      </div>
+      <div className="ep-img">
+        <img src={post.image} alt="" />
+      </div>
+      <div className="ep-desc">
+        <p>{post.desc}</p>
+      </div>
+      <div className="ep-thoughts">
+        <AddThought />
+        <Thoughts />
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
--- pages/api/likes/[likedid].js
+++ pages/api/likes/[likedid].js
@@ -2,8 +2,6 @@ import { databases } from '../../../appwrite';
 import { getSession } from "next-auth/react";
 
 export default async function handler(req, res) {
-
-
   const { likedid } = req.query;
 
   try {
@@ -11,34 +9,25 @@ export default async function handler(req, res) {
       const promise = databases.getDocument('646ed509771c8bf97447', '646ed512bc1b4def6d45', likedid);
       const response = await promise;
       res.send(response.likes);
-    } 
-    else if (req.method === 'POST') {
-const session = await getSession({ req });
+    } else if (req.method === 'POST') {
+      // const session = await getSession({ req });
 
-  console.log(session);
+      // if (!session) {
+      //   return res.status(401).json({ error: "Not Authenticated" });
+      // }
 
-  if (!session) {
-    return res.status(401).json({ error: "Not Authenticated" });
-  }
-
-  const likedby = session.user.email;
+      const likedby = req.body.likedby; // Access the likedby value from req.body
       const docPromise = databases.getDocument('646ed509771c8bf97447', '646ed512bc1b4def6d45', likedid);
       const doc = await docPromise;
       let emails = doc.likes;
-      // data must be passed as form data
-      console.log(emails);
 
       if (emails.includes(likedby)) {
-        console.log("The email exists in the array.");
         const updatedemails = emails.filter((email) => email !== likedby);
-        console.log(updatedemails);
 
         const updatePromise = databases.updateDocument('646ed509771c8bf97447', '646ed512bc1b4def6d45', likedid, { likes: updatedemails });
         const response = await updatePromise;
         res.send({ msg: "done" });
       } else {
-        console.log("The email does not exist in the array.");
-
         const updatePromise = databases.updateDocument('646ed509771c8bf97447', '646ed512bc1b4def6d45', likedid, { likes: [...doc.likes, likedby] });
         const response = await updatePromise;
         res.send(response);
