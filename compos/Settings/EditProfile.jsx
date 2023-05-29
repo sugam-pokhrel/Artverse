9e091e5f0b10b28d800da2d66c1855534fa0b75d
meprazhant <meprazhant@gmail.com>
2023-05-29 12:48:37 +0545

    added more to user profile edit page

--- compos/Settings/EditProfile.jsx
+++ compos/Settings/EditProfile.jsx
@@ -1,6 +1,48 @@
 import React from 'react'
 import { useSession } from 'next-auth/react'
 
+function Social(a) {
+    console.log(a.a)
+    var social = a.a
+    var iconval = ""
+    var icons = {
+        'facebook': 'https://th.bing.com/th/id/OIP.hGaetDAQWapgIJbIOhPhXwHaHa?pid=ImgDet&rs=1',
+        'twitter': 'https://th.bing.com/th/id/R.3f7189662f19f8318fc75252deee723a?rik=Qa956Np1tp8Zcg&riu=http%3a%2f%2f1000logos.net%2fwp-content%2fuploads%2f2017%2f06%2fTwitter-Logo.png&ehk=6ekNd2ZmhpvFDGRZF19QcumP9fb8pZRkwrbFbK%2bpULA%3d&risl=&pid=ImgRaw&r=0',
+        'instagram': 'https://freepngimg.com/thumb/computer/68394-computer-instagram-icons-png-file-hd.png',
+        'linkedin': 'https://th.bing.com/th/id/R.c3fcc390427d68b89647e834adf7fc87?rik=5W%2fa9g9fys8WDg&pid=ImgRaw&r=0',
+        'youtube': 'https://hdclipartall.com/images/youtube-clipart-youtube-square-icons-1600x1600.png',
+        'github': 'https://th.bing.com/th/id/R.7aaf70cb7b91181819e62720db9e4498?rik=3yIi20%2beDqXgdw&pid=ImgRaw&r=0',
+        'pinterest': 'https://th.bing.com/th/id/R.6e9e33c516506bb8b599a2b6b8b55b4c?rik=F%2ftQNhVjNOkX4Q&pid=ImgRaw&r=0',
+        "tiktok": 'https://th.bing.com/th/id/R.4a691e981276229f1c6bd2117efc82ab?rik=jjHjItPM2%2bCP0A&pid=ImgRaw&r=0',
+        "messenger": "https://th.bing.com/th/id/R.a2d9a1da2c43ae5a0545a0c28d15b67a?rik=9GjiRln4TkgiGQ&pid=ImgRaw&r=0"
+    }
+    // remove www. and .com from domain
+    social = social.replace('www.', '')
+    social = social.replace('.com', '')
+    social = social.replace('.in', '')
+    social = social.replace('.co', '')
+    social = social.replace('.org', '')
+    social = social.replace('.net', '')
+    social = social.replace('.edu', '')
+    social = social.replace('.gov', '')
+    social = social.replace('.io', '')
+    social = social.replace('.ai', '')
+    social = social.replace('.me', '')
+    // lowercase
+    social = social.toLowerCase()
+    // check if social is in icons
+    if (social in icons) {
+        iconval = icons[social]
+    } else {
+        iconval = 'https://th.bing.com/th/id/R.53c27d1ef4ab5c32521eccf5d1061397?rik=212WzEK%2fXS9XTQ&pid=ImgRaw&r=0'
+    }
+    return (
+        <div className="ep-soc-">
+            <img src={iconval} alt="icons" />
+        </div>
+    )
+}
+
 function Edit() {
     var session = useSession()
     var [auth, setAuth] = React.useState(false)
@@ -29,28 +71,42 @@ function Edit() {
         } else {
             e.style.border = '1px solid red'
         }
-        var domain = url.split('/')[2]
-        // setDomain(domain)
-        var afterDomain = url.trim().substring(domain.length + 8)
-        // setUsername(afterDomain)
+        try {
+            var domain = url.split('/')[2]
+            setDomain(domain)
+
+            var afterDomain = url.trim().substring(domain.length + 8)
+            setUsername(afterDomain)
+        } catch (error) {
+            // 
+        }
+
     }
+    const [showSocial, setShowSocial] = React.useState(false)
     function submitSocial() {
-        var socials = socials
-        socials.push({ domain: domain, username: username })
-        setSocials(socials)
-        setDomain('')
-        setUsername('')
+        if (!!domain && !!username) {
+
+            setSocials([...socials, { domain: domain, username: username }])
+            setDomain('')
+            setUsername('')
+            setShowSocial(false)
+        } else {
+            alert("Your URL isnt Valid")
+        }
+    }
+    function showAddSocial() {
+        setShowSocial(true)
     }
     return (
         <div className="ep-home">
-            {/* <div className="ep-social">
+            {(showSocial) && <div className="ep-social">
                 <div className="socialAdd">
                     <input type="text" onChange={(e) => checkifWebsite(e.target)} placeholder='Enter or paste your link' />
                     <button className='btn btn-primary' onClick={submitSocial}>Add</button>
-                    <button className='btn btn-warning'>Cancel</button>
+                    <button className='btn btn-warning' onClick={() => setShowSocial(false)}>Cancel</button>
 
                 </div>
-            </div> */}
+            </div>}
             <div className="editProfile__header">
                 <div className="ep-image">
                     <img src={session.data.user.image} alt="" referrerPolicy='no-referrer' />
@@ -104,7 +160,28 @@ function Edit() {
                 </div>
                 <div className="ep-etext">
                     <p>Socials Medias</p>
-                    <button className='btn btn-base'>+ Add</button>
+                    <button className='btn btn-base' onClick={showAddSocial}>+ Add</button>
+                    {(socials.length > 0) && <div className="ep-socials">
+                        {socials.map((social, index) => {
+                            return (
+                                <div className="ep-soc-card" key={index}>
+                                    <div className="ec-left">
+                                        <Social a={social.domain} />
+                                        <p>{social.domain} - {social.username}</p>
+                                    </div>
+                                    <div className="ep-del">
+                                        <button className='btn btn-primary bg-red-800'>Remove</button>
+                                    </div>
+                                </div>
+                            )
+                        })
+                        }
+                    </div>}
+                </div>
+                <div className="ep-etext">
+                    {/* save */}
+                    <button className='btn btn-primary'>Save Changes</button>
+
                 </div>
             </div>
         </div>
