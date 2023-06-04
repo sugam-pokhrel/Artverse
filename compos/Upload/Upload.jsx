8a2c1d8397acc99c6dce0721605becaa51299b17
meprazhant <meprazhant@gmail.com>
2023-06-04 21:30:08 +0545

    made uncomment

--- compos/Upload/Upload.jsx
+++ compos/Upload/Upload.jsx
@@ -73,7 +73,7 @@ function Upload(props) {
 
     return (
         <>
-            {/* {(page == 1) && <div className='hero h-screen flex items-center flex-col justify-center '>
+            {(page == 1) && <div className='hero h-screen flex items-center flex-col justify-center '>
                 <h2 className='text upload-title md:text-2xl text-sm font-bold py-10'> <span>1/2</span> Add a suitable thumbnail of your project.</h2>
                 <div className="flex md:w-3/5 w-11/12 h-3/5  border border-dotted">
                     <div className="flex flex-col upload hover:bg-primary ease-out duration-300" onClick={openFile}>
@@ -87,14 +87,14 @@ function Upload(props) {
                 <h2 className='text upload-title md:text-2xl text-xl font-bold py-10'> <span>2/2</span> Add title and Description about your project.</h2>
                 <div className="flex md:w-3/5 w-11/12 h-5/6  border border-dotted">
                     <div className="flex py-10 gap-3 flex-col upload ease-out duration-300" >
-                        <input onChange={e=>{textChanged(e)}} name='title' required type="text" className='focus:bg-base-300 p-5 rounded-none outline-none border-none input bg-gradient-to-r from-transparent w-full to-transparent uploadInput' placeholder='Give a suitable Title' />
+                        <input onChange={e => { textChanged(e) }} name='title' required type="text" className='focus:bg-base-300 p-5 rounded-none outline-none border-none input bg-gradient-to-r from-transparent w-full to-transparent uploadInput' placeholder='Give a suitable Title' />
                         <img className='w-100 h-3/6' src={URL.createObjectURL(image)} alt="" />
-                        <textarea onChange={e=>{textChanged(e)}} required  name="desc" id="upload-textarea" className='text focus:bg-base-300 p-5' placeholder='Describe your Project under 2000 words, add Hasgtags for better reach' cols="30" rows="10"></textarea>
-                        <button onClick={(e)=>{uploadImage(e)}} className='bg-primary text-white py-2 px-4 rounded-md'>Upload</button>
+                        <textarea onChange={e => { textChanged(e) }} required name="desc" id="upload-textarea" className='text focus:bg-base-300 p-5' placeholder='Describe your Project under 2000 words, add Hasgtags for better reach' cols="30" rows="10"></textarea>
+                        <button onClick={(e) => { uploadImage(e) }} className='bg-primary text-white py-2 px-4 rounded-md'>Upload</button>
                     </div>
                 </div>
             </div>
-            } */}
+            }
             <div className='hero h-screen flex items-center flex-col justify-center '>
                 <h2 className='text upload-title md:text-3xl text-red-300 text-sm font-bold py-10'> Some error occured while uploading, it will be fixed soon. Thanks. <span>ERRTPYE: CORS</span></h2>
             </div>
