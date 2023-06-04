99036dde4caac29416ccffc96f99bbedfb0da605
meprazhant <meprazhant@gmail.com>
2023-06-04 22:45:20 +0545

    made linkify seo friendly

--- pages/linkify/[username].jsx
+++ pages/linkify/[username].jsx
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router'
 import React, { useEffect } from 'react'
 import Linkcard from '../../compos/linkify/Linkcard'
+import Head from 'next/head'
 
 function Linkify() {
     var router = useRouter()
@@ -34,6 +35,27 @@ function Linkify() {
     }
     return (
         <div>
+            <Head>
+                <title>Linkify - Share Links</title>
+                <meta name="description" content="Share your links with Linkify" />
+                <link rel="icon" href="/favicon.ico" />
+
+                <meta property="og:title" content="Linkify - Share Links" />
+                <meta property="og:description" content="Share your links with Linkify" />
+                <meta property="og:image" content="https://i.ibb.co/M8DJntr/image.png" />
+                <meta property="og:url" content="https://artverse.vercel.app/" />
+                <meta property="og:type" content="website" />
+                <meta property="og:site_name" content="Artverse" />
+
+                <meta name="twitter:title" content="Linkify - Share Links" />
+                <meta name="twitter:description" content="Share your links with Linkify" />
+                <meta name="twitter:image" content="https://i.ibb.co/M8DJntr/image.png" />
+                <meta name="twitter:card" content="summary_large_image" />
+
+                <meta name="theme-color" content="#000000" />
+
+
+            </Head>
             {(valid === false) && <div className="explore-load">
                 <h1>404 - Not Found</h1>
                 <h2>No user with username <span className='text text-info'>{router.query?.username}</span> is found, please check and try again</h2>
