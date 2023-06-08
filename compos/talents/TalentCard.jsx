5eea16596c7cc859894e8fcb56dbcb1fae1e234a
Sugam Pokhrel <114973396+sugam-pokhrel@users.noreply.github.com>
2023-06-08 07:11:34 +0545

    made the talent card more fast by using useMemo

--- compos/talents/TalentCard.jsx
+++ compos/talents/TalentCard.jsx
@@ -1,65 +1,73 @@
-import { useSession } from 'next-auth/react'
-import React, { useEffect, useState } from 'react'
-import { useRouter } from 'next/router'
+import { useSession } from 'next-auth/react';
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 import Login from '../Login/Login';
 
 function TalentCard({ data }) {
+  const [isFollowed, setIsFollowed] = useState(false);
+  const [auth, setAuth] = useState(false);
+  const [followCount, setFollowCount] = useState(data?.followers.length);
+  const [isLoading, setIsLoading] = useState(true);
 
+  const session = useSession();
+  const router = useRouter();
 
-    const [isFollowed, setIsFollowed] = useState(false);
-    var [auth, setAuth] = useState(false)
-
-   
-    var session = useSession()
-    if(!session){
-        return <Login />
+  useEffect(() => {
+    if (session?.status === 'authenticated') {
+      setAuth(true);
     }
-    var router = useRouter()
-    useEffect(() => {
-        if (session.status === 'authenticated') {
-            setAuth(true)
-        }
-    }, [session])
-      useEffect(() => {
+  }, [session]);
 
-    var url = "/api/follow/" + data?.email;
-    // declare the data fetching function
-    const fetchData = async () => {
-      const data = await fetch(url);
-      const json = await data.json();
-      console.log(json)
+  const fetchData = async () => {
+    if (!session || !data?.email) return;
+
+    try {
+      const url = '/api/follow/' + data.email;
+      const response = await fetch(url);
+      const json = await response.json();
+      console.log(json);
       const emailExists = json.follow.includes(session.data.user.email);
-
-      if (emailExists) {
-        console.log("The email exists in the object.");
-        setIsFollowed(true)
-      } else {
-        console.log("The email does not exist in the object.");
-      }
-
+      setIsFollowed(emailExists);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
     }
+  };
 
-    // call the function
-    fetchData()
-      // make sure to catch any error
-      .catch(console.error);
-  }, [])
-console.log(session.data.user.email)
+  useEffect(() => {
+    setIsLoading(true);
+    fetchData();
+  }, [session, data?.email]);
 
-  function followuser() {
-    setIsFollowed(!isFollowed); var url = "/api/follow/" + data.email;
+  useEffect(() => {
+    const getPosts = () => {
+      fetch('/api/post/getpostbyEmail?email=' + data.email)
+        .then((res) => res.json())
+        .then((data) => {
+          setLength(data.post.length);
+        });
+    };
+
+    getPosts();
+  }, [data.email]);
+
+  function followUser() {
+    setFollowCount((prevCount) => (isFollowed ? prevCount - 1 : prevCount + 1));
+    setIsFollowed((prevIsFollowed) => !prevIsFollowed);
+
+    const url = '/api/follow/' + data.email;
     if (auth) {
       fetch(url, {
-        method: "POST",
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ likedby: session.data.user.email }), // Pass session email in the request body
+        body: JSON.stringify({ likedby: session.data.user.email }),
       })
         .then((res) => {
           const contentType = res.headers.get('content-type');
           if (contentType && contentType.includes('application/json')) {
-
             return res.json();
           } else {
             return 0;
@@ -67,50 +75,78 @@ console.log(session.data.user.email)
         })
         .then((data) => {
           console.log(data);
-          setIsFollowed(!isFollowed);
-        }
-        )
+        });
     }
   }
 
+  var [length, setLength] = useState(0);
 
-    var [length, setLength] = useState(0)
-    function getPosts() {
-        fetch('/api/post/getpostbyEmail?email=' + data.email)
-            .then(res => res.json())
-            .then(data => {
-                setLength(data.post.length)
-            })
-    }
-    useEffect(() => {
-        getPosts()
-    }, [])
+  function navigateToUser() {
+    var email = data.email;
+    var username = email.slice(0, email.indexOf('@'));
+    router.push('/u/' + username);
+  }
 
-    function navtouser() {
-        // slice email and make username
-        var email = data.email
-        var username = email.slice(0, email.indexOf('@'))
-        router.push('/u/' + username)
-    }
-
-    return (
-        <div className='talent-card'>
-            <div className="tc-data">
-                <div className="tc-img">
-                    <img src={data.image} onClick={navtouser} alt={data.name} referrerPolicy='no-referrer' />
-                </div>
-                <div className="tc-name">
-                    <p className='text-2xl font-bold' onClick={navtouser}>{data.name}</p>
-                    <p className='text-xl'>{data?.profession || "Student"} . {data?.followers.length} Followers</p>
-                    <p className='text-xl'>{length} Projects . 0 Blogs</p>
-                </div>
+  if (!session) {
+    return <Login />;
+  }
+      {!isLoading && (
+        <>
+          <div className='tc-data'>
+            <div className='tc-img'>
+              <img src={data.image} onClick={navigateToUser} alt={data.name} referrerPolicy='no-referrer' />
             </div>
-            <div className="tc-fc">
-                {(auth) && <div className="fc-btn btn btn-primary" onClick={followuser}>{isFollowed ? 'Unfollow' : 'Follow'} </div>}
-                <div className="fc-btn btn btn-warning" onClick={navtouser}>Visit Profile</div>
+            <div className='tc-name'>
+              <p className='text-2xl font-bold' onClick={navigateToUser}>
+                {data.name}
+              </p>
+              <p className='text-xl'>
+                {data?.profession || 'Student'} . {followCount} Followers
+              </p>
+              <p className='text-xl'>{length} Projects . 0 Blogs</p>
             </div>
+          </div>
+          <div className='tc-fc'>
+            {auth && (
+              <div className='fc-btn btn btn-primary' onClick={followUser}>
+                {isFollowed ? 'Unfollow' : 'Follow'}
+              </div>
+            )}
+            <div className='fc-btn btn btn-warning' onClick={navigateToUser}>
+              Visit Profile
+            </div>
+          </div>
+        </>
+      )}
+
+  return (
+    <div className='talent-card'>
+      <div className='tc-data'>
+        <div className='tc-img'>
+          <img src={data.image} onClick={navigateToUser} alt={data.name} referrerPolicy='no-referrer' />
         </div>
-    )
+        <div className='tc-name'>
+          <p className='text-2xl font-bold' onClick={navigateToUser}>
+            {data.name}
+          </p>
+          <p className='text-xl'>
+            {data?.profession || 'Student'} . {followCount} Followers
+          </p>
+          <p className='text-xl'>{length} Projects . 0 Blogs</p>
+        </div>
+      </div>
+      <div className='tc-fc'>
+        {auth && (
+          <div className='fc-btn btn btn-primary' onClick={followUser}>
+            {isFollowed ? 'Unfollow' : 'Follow'}
+          </div>
+        )}
+        <div className='fc-btn btn btn-warning' onClick={navigateToUser}>
+          Visit Profile
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default TalentCard
+export default TalentCard;
