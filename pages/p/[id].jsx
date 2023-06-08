89ee55c6c11fe20ce232bbed76c4c2a8b12ca7b8
meprazhant <meprazhant@gmail.com>
2023-06-08 20:27:58 +0545

    made p page ssr friendly

--- pages/p/[id].jsx
+++ pages/p/[id].jsx
@@ -5,12 +5,38 @@ import { useSession } from 'next-auth/react';
 import Login from '../../compos/Login/Login';
 import Head from 'next/head';
 
-function PostId() {
+
+export async function getServerSideProps(context) {
+    var postid = null
+    var newData = null
+    try {
+        postid = await context.query.id
+        await fetch(`https://artverses.vercel.app/api/post/${postid}`)
+            .then(res => res.json())
+            .then(data => {
+                newData = data
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    } catch (error) {
+        console.log(error)
+    }
+    return {
+        props: {
+            newData,
+        }
+    }
+}
+
+function PostId({ newData }) {
     const { data: session } = useSession();
     const router = useRouter();
     const [postData, setPostData] = useState({});
     const [userData, setUserData] = useState({});
     const [loading, setLoading] = useState(true);
+
+    console.log(newData)
 
     useEffect(() => {
         if (router.query.id) {
@@ -42,19 +68,19 @@ function PostId() {
     return (
         <>
             <Head>
-                <title>{postData.title} | ArtVerse</title>
-                <meta name="description" content={postData.description} />
+                <title>{newData.title} | ArtVerse</title>
+                <meta name="description" content={newData.desc} />
 
-                <meta property="og:title" content={postData.title} />
-                <meta property="og:description" content={postData.description} />
-                <meta property="og:image" content={postData.thumbnail} />
-                <meta property="og:url" content={"https://artverse.vercel.app/p/" + postData._id} />
+                <meta property="og:title" content={newData.title} />
+                <meta property="og:description" content={newData.desc} />
+                <meta property="og:image" content={newData.image} />
+                <meta property="og:url" content={"https://artverse.vercel.app/p/" + newData.$id} />
                 <meta property="og:type" content="website" />
                 <meta property="og:site_name" content="Artverse" />
 
-                <meta name="twitter:title" content={postData.title} />
-                <meta name="twitter:description" content={postData.description} />
-                <meta name="twitter:image" content={postData.thumbnail} />
+                <meta name="twitter:title" content={newData.title} />
+                <meta name="twitter:description" content={newData.description} />
+                <meta name="twitter:image" content={newData.image} />
                 <meta name="twitter:card" content="summary_large_image" />
             </Head>
             {loading ? (
